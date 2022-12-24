10c906c651d38457597df522e48d1cd4394b9ad9
Raul Quintero <raulquintero13@gmail.com>
2022-12-23 19:38:37 -0800

    creating blog sp

--- README.md
+++ README.md
@@ -2,26 +2,15 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.4.
 
+## Ejercicio Comlementario curso Angular
+Crea una simple page (es decir, una página única) que imite a un blog y tenga las siguientes características: Menú horizontal que al colapsar se convierta en versión móvil funcional gracias a JavaScript Un botón para registrarte donde al presionar se abre un pop up con un formulario para registrarse.
+
+## Sitio Web
+https://raulquintero13.github.io/ang-blog-sp/
+
+## Screenshot
+https://raulquintero13.github.io/ang-blog-sp/assets/images/screenshot01.jpg
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-blog-sp';
+  blogs:any = []; 
+
+  constructor(){
+    this.blogs = this.getBlogs();
+    console.log('constructor',this.getBlogs());
+  }
+  
+  
+
+  getBlogs(){
+
+    return [
+      {id: 1, image: 'noruega_top.jpg',title:'La primavera en Noruega', 
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum lectus. Donec ullamcorper est lectus, a sodales tortor euismod', 
+        author: 'juan perez', date: '10/12/2003'},
+      {id: 2, image: 'brasil_top.jpg',title:'Conociendo Brasil', 
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum lectus. Donec ullamcorper est lectus, a sodales tortor euismod', 
+        author: 'juan perez', date: '10/12/2003'},
+      {id: 3, image: 'world_top.jpg',title:'Top 10 Mejores destinos', 
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum lectus. Donec ullamcorper est lectus, a sodales tortor euismod',
+        author: 'juan perez', date: '10/12/2003'},
+      {id: 4, image: 'baja_top.jpg',title:'Conoce la Baja', 
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum lectus. Donec ullamcorper est lectus, a sodales tortor euismod',
+        author: 'juan perez', date: '10/12/2003'},
+      {id: 5, image: 'primer_top.jpg',title:'Mi primer Blog', 
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum lectus. Donec ullamcorper est lectus, a sodales tortor euismod',
+        author: 'juan perez', date: '10/12/2003'},
+    ]
+
+  }
 }
